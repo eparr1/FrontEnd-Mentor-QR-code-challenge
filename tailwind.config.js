655b58c73c040307b19baf99b90cf539a65c2be8
Emma Code's <emmaparr12345@gmail.com>
2024-08-09 11:06:09 +0100

    all files

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily:{
+        Outfit:['Outfit', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+
+  
+}
+
